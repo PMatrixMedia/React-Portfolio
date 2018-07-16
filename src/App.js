128ed6f67f3f6c28e2fa06807f5888495179d79e
Christopher Faison <pmatrixmedia@gmail.com>
2018-07-16 06:20:34 -0500

    added routes tweaking UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Button from 'antd/lib/button';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <button type="button" class="btn btn-outline-warning" btn-lg>Enter Phase Matrix Media</button>
+        <Frame>
+            <Router>
+                <div>
+                <Route path="/start" component={Start} />
+                <Route path="/home" component={Home} />
+                <Route path="/About" component={About} />
+                <Route path="/Contact" component={Contact} />
+                </div>
+            </Router>
+        </Frame>
+      </div>
+    );
+  }
+}
             </div>
         );
     }
