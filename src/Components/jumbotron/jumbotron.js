759142af4529998f699470ea72004cac4653a122
PMatrixMedia <33465098+PMatrixMedia@users.noreply.github.com>
2018-08-07 22:36:23 -0500

    changed to jumbotron

--- src/Components/jumbotron/jumbotron.js
+++ src/Components/jumbotron/jumbotron.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './style.css';
-import './animation.js';
+import scrolltext from './animation.js';
 
 
 
 
 
 const jumbotron = () =>
+  
   <div className="jumbotron jumbotron-fluid">
+  <React.Fragment>
+    <scrolltext>
                   <div class="scrolling-text st-regular" id="firstText"><span class="copy-repeater">Node.React.Bootstrap.JQuery; Express. Powershell. Azure.SCCM.Developer</span></div>
                   <div class="scrolling-text st-inverse" id="Secondinverse"><span class="copy-repeater">Welcome to My Site.My name is Chris and I am a System Engineer. Administrator. Developer.Enthusiast .</span></div>
                   <div class="scrolling-text st-regular" id ="thirdText"><span class="copy-repeater">Microsoft.Android.MSSql.MySQL.Windows Server.Windows.Azure.Developer</span></div>
+    </scrolltext>
+    </React.Fragment>
   </div>;
 
 
