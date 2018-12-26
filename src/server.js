704013bc069f842a4cc0b3cfe54446bc2fd1ad56
Christopher Faison <pmatrixmedia@gmail.com>
2018-12-26 08:02:18 -0600

    IIS intergration w/ express

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 // Express app setup
-const app = express();
+const path = require('path');
+const app = express.createserver();
 
 app.use(express.static("Pages"));
 
 app.use(express.static("Components"));
 
 app.use(express.static("images"));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+-app.get('/node/express/myapp/foo', function (req,res) {
++app.get('/node/express/myapp/bar', function(req,res) {
+    res.sendfile(path.join(__dirname, 'build', 'index.html'));
+})});
+
+app.listen(9000);
